a52e6b526564fc117c5cf765800ad805e9e3c906
Abhishek <abhitiwari3010@gmail.com>
2023-11-05 07:39:24 +0530

    added bid route

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth";
 import todoRoutes from "./routes/activities";
 import paymentRoutes from "./payment/createCheckoutSession"
 import eventRoutes from './payment/webhook';
+import bidRoutes from './payment/bid';
 import cors from "cors";
 import { connectionString, port } from "./constants";
 
@@ -14,6 +15,7 @@ app.use("/auth", express.json(), authRoutes);
 app.use("/todo", express.json(), todoRoutes);
 app.use("/pay", express.json(), paymentRoutes);
 app.use("/event",eventRoutes)
+app.use("/bid",express.json(),bidRoutes);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
