0501f3f064344fd0c02a0a43a2a6c017232e00ad
Abhishek <abhitiwari3010@gmail.com>
2023-11-19 19:45:05 +0530

    added buy button functionalities

--- apps/client/src/components/visitor/Home.tsx
+++ apps/client/src/components/visitor/Home.tsx
@@ -1,30 +1,34 @@
 import { useRecoilState, useRecoilValue } from "recoil";
 import imageLink from "../../assets/imageLink";
-import {userData} from "../../data/ComponentData"
+import {productListState, userData} from "../../data/ComponentData"
 import axios from "axios";
 import { useEffect } from "react";
 import AddMoneyIcon from '../../assets/svg/AddMoneyIcon.svg';
-import {  activeListItemstate, logoutState } from "../../data/RelatedStates";
+import {  activeListItemstate, buyProductState, buyState, logoutState } from "../../data/RelatedStates";
 import MenuList from "./MenuList";
 import { VerticalLine } from "../common";
 import { DisplayArea } from "./Display";
 import mongoose from "mongoose";
 import { Navigate, useNavigate } from "react-router-dom";
-
-
+import UserAccount from './Account';
+const {ProfilePhoto}=UserAccount;
 
 function Home()
 {
     const [user,setUser] = useRecoilState(userData)
     const [isDialogOpen, setDialogOpen] = useRecoilState(logoutState);
+    const [isBuyPressed, setBuyPressed] = useRecoilState(buyState);
     const navigate = useNavigate();
     //This will close the opened Dialog box of logout...
     const closeDialog = () => {
         setDialogOpen(false);
+        setBuyPressed(false);
       };
 
     //This will logout the user...
     function logoutUser(){
+      console.log("logedout");
+      
         localStorage.removeItem('token');
         console.log(localStorage.getItem('token'))
         navigate('/auth');
@@ -40,11 +44,73 @@ function Home()
     productId: []
        })
         closeDialog();
+
     }
 
+    interface Product {
+      _id:string;
+      name: string;
+      description: string;
+      originalPrice: number;
+      image: string;
+      maxBid: { type: number; required: true };
+      minBid: { type: number; required: true };
+      sold: boolean;
+      sellPrice: number;
+      sellerId: string;
+      buyerId: string;
+      bids: Bid[];
+    }
+
+    interface Bid {
+      amount: number,
+      userId: mongoose.Types.ObjectId,
+    }
+
+    const [productList,setProductList]=useRecoilState(productListState)
+    
+    
+    const [productId,setProductId] = useRecoilState(buyProductState)
+    async function buyAProduct()
+    {
+      const authorization = "bearer "+localStorage.getItem('token');
+      console.log(authorization);
+      console.log("this is product Id");
+      
+      console.log(productId);
+      
+      const response = await axios.put(`http://localhost:4242/general/buy/${productId}`,{},{
+        headers:{
+          Authorization:authorization
+        }
+          });
+          console.log(response.status);
+        if(response.status===200)
+        {
+          let array = [...productList];
+          
+          let index = array.findIndex((element:Product)=>element._id==productId);
+          array.forEach((element:Product)=>{
+            if (element._id === productId) {
+              setUser(prevUser => {
+                return {
+                  ...prevUser,
+                  balance: Number(prevUser.balance) - Number(element.maxBid)
+                };
+              });
+            }
+          })
+          
+          array.splice(index,1);
+          setProductList(array);
+          closeDialog();
+        }
+        else{
+          console.log(response.data)
+        }
+    }
     //This will get user's data
      function getUserData() {
-        
         let token = localStorage.getItem('token');
         let authString="";
         if(token){
@@ -73,32 +139,30 @@ function Home()
        
     },[])
 
+    const [activeListItem,setActiveListItem] = useRecoilState(activeListItemstate)
 
-
+    
     
     return <div>
         <nav>
-            <span ><img style={{height:"7vh", width:"7vh", borderRadius:"50%",objectFit:"cover", backgroundColor:"red"}} src={user.imageLink.toString()} alt="" /></span>
+            <ProfilePhoto user={user} dimention={7}/>
             <span>{user.username.toString()}</span>
             <span><input type="text" name="" id="" placeholder="Hi Abhishek , search for a product here..."/></span>
-            <span>{user.balance.toString()} INR<a> <img src={AddMoneyIcon} alt="Add Money" /></a></span>
-
+            <span>{user.balance.toString()} INR<img onClick={()=>setActiveListItem(5)} src={AddMoneyIcon} alt="Add Money" /></span>
         </nav>
     <main>
-    <button style={{display:"inline"}}>Sell Your Product</button>
+    <button onClick={()=>navigate("/addAProduct")} style={{display:"inline"}}>Sell Your Product</button>
     <div style={{height:"80%", display:"block",}}>
-    <MenuList/>
-    
+    <MenuList/>  
     <VerticalLine/>
-     
     <DisplayArea/>
-    {isDialogOpen && (
+    {(isDialogOpen||isBuyPressed) && (
         <div className="overlay">
           <div className="modal">
             <div className="modal-content">
-                <p>Do you want to logout of Bidhub?</p>
+                {isDialogOpen?<p>Do you want to logout of Bidhub?</p>:<p>Do you want to buy this product at Max Bid Price?</p>}
               <span className="close" onClick={closeDialog}>Close</span>
-            <span className="logout" onClick={logoutUser}>Logout</span>
+            {isDialogOpen?<span className="logout" onClick={logoutUser}>Logout</span>:<span className="buy" onClick={buyAProduct}>Buy</span>}
             </div>
           </div>
         </div>
@@ -106,6 +170,21 @@ function Home()
     </div>
     </main>
     </div>
+}
+type userType = {
+  userId: string;
+  username: string;
+  password: string;
+  balance: number;
+  fullName: string;
+  hostelName: string;
+  hostelRoom: string;
+  imageLink: string;
+  productId: never[];}
+
+type propsType = {
+  user:userType;
+  dimention:number;
 }
 
 export default Home;
