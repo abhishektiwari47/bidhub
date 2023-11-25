d0eb13fc5e47459843dd89bfcc947fdd9984310e
Abhishek <abhitiwari3010@gmail.com>
2023-11-25 20:40:28 +0530

    designed add money

--- apps/client/src/components/visitor/AddMoney.tsx
+++ apps/client/src/components/visitor/AddMoney.tsx
@@ -32,30 +32,34 @@ function AddMoney(){
       }
     }
 
-    return <div>
-    <div style={{height:"100%", backgroundColor:"yellow" ,verticalAlign:"top"}}>
-    <p>Your current Bidhub wallet balance is</p>
-    <p>{user.balance} INR</p>
+    return <div >
+    <div className="p-5 lg:mx-10 my-2 border-solid rounded-lg border-black border-[1px]" style={{height:"100%", backgroundColor:"" ,verticalAlign:"top"}}>
+    <p className="lg:text-[30px]">Your current Bidhub wallet balance is</p>
+    <p className="lg:text-[30px] text-[#FF6B00] font-medium">{user.balance} INR</p>
     </div>
-    <div style={{height:"70%",backgroundColor:"#ffffff",}}>
-    <p>Add money to Bidhub Wallet</p>
-    <p style={{border:"solid 1px #717171",borderRadius:"2px"}}>INR <input value={inputAmount} placeholder="100" type="number" style={{border:"none",outline:"none", borderBottom: '0px'} } onChange={(event)=>{setInputAmount(Number(event.target.value))}}/></p>
+    {/* second container */}
+    <div className="p-5 h-[50vh] lg:h-[45vh] bg-[#EEEEEE] lg:mx-10 my-5 border-solid rounded-lg border-black border-[1px]">
+    <div className="m-5">
+    <p  className="text-[24px] my-2">Add money to Bidhub Wallet</p>
+    <p className="bg-white p-2 text-[24px] text-[#808080] " style={{border:"solid 1px #717171",borderRadius:"2px"}}>INR <input className="text-[#515151] px-5 w-[90%]" value={inputAmount} type="text"  style={{border:"none",outline:"none", borderBottom: '0px'} }
+
+    onChange={(event)=>{setInputAmount(Number(event.target.value))}}/></p>
     </div>
-    <span><SelectAmount value={100} /></span>
+    <div className="m-5">
+    <span ><SelectAmount value={100} /></span>
     <span><SelectAmount value={200} /></span>
     <span><SelectAmount value={500} /></span>
-    <span><SelectAmount value={1000} /></span>
-    <span><SelectAmount value={10000} /></span>
-    <div><button onClick={()=>{console.log(inputAmount);pay();}}>Proceed</button></div>
-    </div>
+    <span className="hidden lg:inline-block"><SelectAmount value={1000} /></span>
+    <div  className='flex my-5 justify-end  text-right ' ><button className="bg-green-600 p-2 text-white rounded shadow" onClick={()=>{console.log(inputAmount);pay();}}>Proceed</button></div>
+    </div></div></div>
 }
 
 function SelectAmount(props:{value:number})
 {
     const [inputAmount,setInputAmount] = useRecoilState(inputAmountState)
-    return <span onClick={()=>setInputAmount(props.value+inputAmount)} style={{width:"10%", backgroundColor:"grey" ,color:"#515151"}}>
+    return <div className="bg-[#D9D9D9] inline-block text-[24px] mx-2 px-3" onClick={()=>setInputAmount(props.value+inputAmount)} style={{color:"#515151"}}>
       +{props.value}
-    </span>
+    </div>
 }
 
 export default AddMoney;
