11cc7e8e7ccefdab0ac16ad599f58ef930f29cf6
Abhishek <abhitiwari3010@gmail.com>
2023-11-30 20:20:23 +0530

    added new route 8

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -17,7 +17,7 @@ app.use("/pay", express.json(), paymentRoutes);
 app.use("/event",eventRoutes)
 app.use("/bid",express.json(),bidRoutes);
 app.get("/done",(req,res)=>{
-    res.json({"message":"this is done now 8"})
+    res.json({"message":"this is done now 9"})
 });
 
 app.listen(3000,"0.0.0.0", () => {
