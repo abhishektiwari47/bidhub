a23310e38035ceaad739c45b66776cfbc4734442
Abhishek <abhitiwari3010@gmail.com>
2023-11-30 17:55:14 +0530

    this is new 2

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -17,7 +17,7 @@ app.use("/pay", express.json(), paymentRoutes);
 app.use("/event",eventRoutes)
 app.use("/bid",express.json(),bidRoutes);
 app.get("/done",(req,res)=>{
-    res.json({"message":"this is done now"})
+    res.json({"message":"this is done now 2"})
 });
 
 app.listen(3000,"0.0.0.0", () => {
