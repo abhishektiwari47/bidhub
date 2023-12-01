2018ab8af2c671fbb5df80049c329edca0735bcb
Abhishek <abhitiwari3010@gmail.com>
2023-12-02 02:20:20 +0530

    added floating button

--- apps/client/src/components/visitor/FloatingButton.tsx
+++ apps/client/src/components/visitor/FloatingButton.tsx
@@ -1,16 +1,25 @@
 import { useState } from "react"
 import { useRecoilState } from "recoil"
 import { isDarkModeState, isFloatingState } from "../../data/RelatedStates"
+import { useNavigate } from "react-router-dom"
 
 function FloatingButton(){
     const [isDark] = useRecoilState(isDarkModeState)
     const [isFloating,setIsFloating] = useRecoilState(isFloatingState)
-    return <div>
-       <div className="overlay" style={{color:(isDark)?"white":"black", display:(isFloating)?"block":"hidden"}}  id="Support">Support</div>
-       <div style={{color:(isDark)?"white":"black", display:(isFloating)?"block":"hidden"}}  id="TermsOfService">Terms Of Service</div>
-       <div style={{color:(isDark)?"white":"black", display:(isFloating)?"block":"hidden"}}  id="PrivacyPolicy">Privacy Policy</div>
-       <div style={{color:(isDark)?"white":"black", display:(isFloating)?"block":"hidden"}}  id="Cencellation">Cencellation</div>
-       <div onChange={()=>setIsFloating(prev=>!prev)} className="bg-[#FF6B00]">?</div>
+    console.log("the value after rebuild");
+    const navigate = useNavigate();
+    console.log(isFloating);
+    
+    return <div className="fixed z-10 right-0 text-center justify-center items-start bottom-0 p-4">
+        <div className="rounded-xl px-2 my-1" style={{backgroundColor:"black"}}>
+       <div className="hover:cursor-pointer" style={{color:"white", display:(isFloating)?"block":"none"}}  id="Support" onClick={()=>navigate("/contactUs")}>Contact</div>
+       <div className="hover:cursor-pointer" style={{color:"white", display:(isFloating)?"block":"none"}}  id="TermsOfService" onClick={()=>navigate("/termsOfService")}>Terms</div>
+       <div className="hover:cursor-pointer" style={{color:"white", display:(isFloating)?"block":"none"}}  id="PrivacyPolicy" onClick={()=>navigate("/privacyPolicy")}>Privacy</div>
+       <div className="hover:cursor-pointer" style={{color:"white", display:(isFloating)?"block":"none"}}  id="Cencellation" onClick={()=>navigate("/refundPolicy")}>Refund</div>
+       </div>
+       
+       <div onClick={()=>{setIsFloating(prev=>!prev);console.log(isFloating);
+       }} className="bg-[#FF6B00] w-7 h-7 text-center justify-center items-center align-middle hover:cursor-pointer flex  rounded-full mx-7 text-white shadow shadow-gray-950"><p>#</p></div>
     </div>
 }
 
