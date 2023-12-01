0f456658bb11f819159a86798ca98401872611ec
Abhishek <abhitiwari3010@gmail.com>
2023-12-01 20:42:22 +0530

    added overlay to floating button

--- apps/client/src/components/visitor/FloatingButton.tsx
+++ apps/client/src/components/visitor/FloatingButton.tsx
@@ -6,7 +6,7 @@ function FloatingButton(){
     const [isDark] = useRecoilState(isDarkModeState)
     const [isFloating,setIsFloating] = useRecoilState(isFloatingState)
     return <div>
-       <div style={{color:(isDark)?"white":"black", display:(isFloating)?"block":"hidden"}}  id="Support">Support</div>
+       <div className="overlay" style={{color:(isDark)?"white":"black", display:(isFloating)?"block":"hidden"}}  id="Support">Support</div>
        <div style={{color:(isDark)?"white":"black", display:(isFloating)?"block":"hidden"}}  id="TermsOfService">Terms Of Service</div>
        <div style={{color:(isDark)?"white":"black", display:(isFloating)?"block":"hidden"}}  id="PrivacyPolicy">Privacy Policy</div>
        <div style={{color:(isDark)?"white":"black", display:(isFloating)?"block":"hidden"}}  id="Cencellation">Cencellation</div>
